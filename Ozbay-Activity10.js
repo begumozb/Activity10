93278fa30629e586d72df997adb047172f22fe8d
begumozb <66123462+begumozb@users.noreply.github.com>
2021-05-30 19:53:35 +0300

    Update Ozbay-Activity10.js

--- Ozbay-Activity10.js
+++ Ozbay-Activity10.js
@@ -14,19 +14,3 @@ var pls = [
 $( "#pl" ).autocomplete({
     source: pls 
     });
-
-/*HTML Validation */
-/*$('input[type="text"]')
-	.on('invalid', function(){
-		return this.setCustomValidity('Please fill this field.');
-	})
-	.on('input', function(){
-		return this.setCustomValidity('');
-	});
-$('input[type="email"]')
-	.on('invalid', function(){
-		return this.setCustomValidity('Please add an @ sign to the email address.');
-	})
-	.on('input', function(){
-		return this.setCustomValidity('');
-	});*/
